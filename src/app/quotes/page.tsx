6a3890bebd245c77004581a4b256963320b9ed07
TranquilVarun <varunverma0623@gmail.com>
2024-02-14 11:19:34 -0800

    something

--- src/app/quotes/page.tsx
+++ src/app/quotes/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 type Props = {};
 
 const QuotingPage = (props: Props) => {
-  return <div>QuotingPage</div>;
+  return <div className="w-full h-full bg-white">QuotingPage</div>;
 };
 
 export default QuotingPage;
