6ea617ceb07c28e0add9adb46828927ecb6c1fa5
TranquilVarun <varunverma0623@gmail.com>
2024-02-19 17:26:49 -0800

    transparent

--- src/components/record/record-table/cell/EnumCell.tsx
+++ src/components/record/record-table/cell/EnumCell.tsx
@@ -36,7 +36,7 @@ export const EnumCell = ({
       clickableComponent={
         <textarea
           autoFocus={isUserCreatedRow && isFirstField}
-          className="block border-l-0 border-t-0 hover:border-l hover:border-t focus:border-t focus:border-l focus:z-10 resize-none cursor-pointer focus:outline-0 focus:border-gray-400 hover:border-gray-400/80 focus:cursor-auto h-7 text-sm border border-1 border-gray-200 w-full py-0.5 px-1 z-0 hover:rounded-md focus:rounded-md"
+          className="bg-transparent block border-l-0 border-t-0 hover:border-l hover:border-t focus:border-t focus:border-l focus:z-10 resize-none cursor-pointer focus:outline-0 focus:border-gray-400 hover:border-gray-400/80 focus:cursor-auto h-7 text-sm border border-1 border-gray-200 w-full py-0.5 px-1 z-0 hover:rounded-md focus:rounded-md"
           value={defaultValue}
         />
       }
