e6de62dcc6a7b1db35c50dde44a49c763c17b2fe
Ethan_Hou1117 <100500816+Ethan-Hou1117@users.noreply.github.com>
2024-03-02 18:04:03 -0800

    loading fast bounce

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,14 +8,17 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      animation: {
+        "fast-bounce": "bounce 0.5s infinite", // Adjust the duration as needed, here it's set to 0.5s
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       width: {
-        '1/7': '14.2857143%', // Adjust the value based on your needs
-        '6/7': '85.7142857%', // Adjust the value based on your needs
+        "1/7": "14.2857143%", // Adjust the value based on your needs
+        "6/7": "85.7142857%", // Adjust the value based on your needs
       },
     },
   },
