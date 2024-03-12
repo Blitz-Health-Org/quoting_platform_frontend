d5c47746d81526c9253477cae90386ca67d57d59
TranquilVarun <varunverma0623@gmail.com>
2024-03-12 12:30:01 -0700

    linted

--- src/components/comparison/RecursiveQuoteColumnDisplay.tsx
+++ src/components/comparison/RecursiveQuoteColumnDisplay.tsx
@@ -53,7 +53,9 @@ export const RecursiveQuoteColumnDisplay = ({
           <hr className="w-full border-b-1 border-gray-500"></hr>
         )}
 
-        <hr className={`w-full border-gray-300 ${field.label === "Plan Name" && isQuoteCard ? "border-t-0" : ""}`}></hr>
+        <hr
+          className={`w-full border-gray-300 ${field.label === "Plan Name" && isQuoteCard ? "border-t-0" : ""}`}
+        ></hr>
         <div className="flex items-center justify-start h-12 w-full text-nowrap overflow-x-auto">
           <p className="ml-4 break-all font-semibold">
             {["string", "number", "boolean"].includes(field.type) ? (
