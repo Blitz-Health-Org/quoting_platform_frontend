e55b35146d26f89d0f4b6ca1145602b7a6e84f75
TranquilVarun <varunverma0623@gmail.com>
2024-03-13 19:54:46 -0700

    oops linted

--- src/app/select/ActionBar.tsx
+++ src/app/select/ActionBar.tsx
@@ -9,12 +9,9 @@ export const ActionBar = ({ actionBarEntries }: ActionBarProps) => {
     <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 z-10 flex items-center h-12 px-2 border border-gray-400 rounded-md bg-gray-200 shadow-lg">
       {actionBarEntries.map((item) => {
         return (
-          <div>
-            <ActionBarItem
-              label={item.label}
-              onClick={item.onClick}
-              key={item.label}
-            />
+          // The key should be added here to the parent `div` element and not to `ActionBarItem`
+          <div key={item.label}>
+            <ActionBarItem label={item.label} onClick={item.onClick} />
           </div>
         );
       })}
