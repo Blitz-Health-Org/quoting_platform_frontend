1b8c4f15315efb7d5010e578402609906f7b21d1
TranquilVarun <varunverma0623@gmail.com>
2024-03-19 12:35:16 -0700

    removed compare cost route

--- src/components/comparison/Subheader.tsx
+++ src/components/comparison/Subheader.tsx
@@ -56,10 +56,12 @@ export const Subheader: React.FC<SubheaderProps> = ({
 
   console.log("client", client);
 
-  const handleCompareCostsClick = () =>
-    router.push(
-      `/cost?clientId=${client.id}&planId=${client.connected_plans[0].id}`,
-    );
+  const handleCompareCostsClick = () => {
+    // router.push(
+    //   `/cost?clientId=${client.id}&planId=${client.connected_plans[0].id}`,
+    // );
+    handleBusiness(1);
+  }
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 h-30 mb-2 mt-12">
