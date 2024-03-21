adb6c6ef42282b200cff1ab9304b3818af422e58
TranquilVarun <varunverma0623@gmail.com>
2024-03-21 10:24:18 -0700

    another test

--- src/app/select/page.tsx
+++ src/app/select/page.tsx
@@ -549,7 +549,7 @@ export default function SelectQuotes() {
 
   return (
     <>
-      <main className="flex w-full h-full overflow-hidden pl-4 md:pl-0 bg-gray-100">
+      <main className="flex w-full h-full overflow-x-scroll pl-4 md:pl-0 bg-gray-100">
         <Navbar selected="Quotes" />
         <div className="w-full md:w-6/7 flex">
           <div className="h-screen overflow-x-scroll flex-col w-full bg-gray-100 bg-opacity-50 pl-2 pr-6 pt-5 pb-6 text-gray-700">
