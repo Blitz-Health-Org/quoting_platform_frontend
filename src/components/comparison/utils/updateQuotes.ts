f6b7a1bf86e391b659d9c593742e45af07d2eaa5
Ethan_Hou1117 <100500816+Ethan-Hou1117@users.noreply.github.com>
2024-03-29 02:26:08 -0700

    update quotes not client.connected_plans

--- src/components/comparison/utils/updateQuotes.ts
+++ src/components/comparison/utils/updateQuotes.ts
@@ -7,29 +7,28 @@ export async function handleUpdateQuotes(
   planId: number,
   fetchClientAndQuotes: any,
 ) {
-  const { data: client, error: clientError } = await supabase
-    .from("clients")
-    .select()
-    .eq("id", clientId)
-    .single();
+  // const { data: client, error: clientError } = await supabase
+  //   .from("clients")
+  //   .select()
+  //   .eq("id", clientId)
+  //   .single();
 
-  if (clientError) {
-    console.error("Error fetching client from Supabase:", clientError);
-    throw clientError;
-  }
+  // if (clientError) {
+  //   console.error("Error fetching client from Supabase:", clientError);
+  //   throw clientError;
+  // }
 
-  const newConnectedPlans = client.connected_plans.map((plan: any) => {
-    if (plan.id === planId) {
-      return { ...plan, selectedQuotes: updatedQuotes };
-    } else {
-      return plan;
-    }
-  });
+  // const newConnectedPlans = client.connected_plans.map((plan: any) => {
+  //   if (plan.id === planId) {
+  //     return { ...plan, selectedQuotes: updatedQuotes };
+  //   } else {
+  //     return plan;
+  //   }
+  // });
 
   const { data, error } = await supabase
-    .from("clients") // Replace 'your_table_name' with your actual table name
-    .update({ connected_plans: newConnectedPlans }) // 'plans' is the array to insert into the 'connected_plans' column
-    .match({ id: clientId }); // Assuming 'selectedClient.id' is the primary key of the row you want to update
+    .from("quotes") // Replace 'your_table_name' with your actual table name
+    .upsert(updatedQuotes); // 'plans' is the array to insert into the 'connected_plans' column
 
   if (error) {
     console.error("Error updating connected plans in Supabase:", error);
